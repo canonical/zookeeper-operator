21bd04c8038d7dc53454100ae760b56e09831170
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2022-06-19 23:38:24 +0100

    test: add cluster/client lib unittests

---
+++ tests/unit/test_client.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import unittest
+from unittest.mock import patch
+
+from charms.zookeeper.v0.client import (
+    MembersSyncingError,
+    QuorumLeaderNotFoundError,
+    ZooKeeperClient,
+    ZooKeeperManager,
+)
+
+
+class DummyClient:
+    def __init__(self, follower=False, syncing=False, ready=True):
+        self.follower = follower
+        self.syncing = syncing
+        self.ready = ready
+        self.SRVR = "Zookeeper version: version\nOutstanding: 0\nMode: leader"
+        self.MNTR = "zk_pending_syncs	0\nzk_peer_state	leading - broadcast"
+
+        if self.follower:
+            self.SRVR = self.SRVR.replace("leader", "follower")
+        if not self.ready:
+            self.MNTR = self.MNTR.replace("broadcast", "other")
+        if self.syncing:
+            self.MNTR = self.MNTR.replace("0", "1")
+
+    def get(self, _):
+        return (b"server.1=bilbo.baggins\nserver.2=sam.gamgee\nnversion=100000000", b"ZnodeStat()")
+
+    def command(self, *args):
+        if args[0] == b"srvr":
+            return self.SRVR
+
+        if args[0] == b"mntr":
+            return self.MNTR
+
+    def reconfig(self):
+        pass
+
+    def connected(self):
+        return True
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+
+class TestClient(unittest.TestCase):
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient())
+    def test_config(self, client):
+        client = ZooKeeperClient(host="", client_port="")
+        result, version = client.config
+        servers = [server.split("=")[0] for server in result]
+
+        self.assertEquals(version, 4294967296)
+        self.assertIn("server.1", servers)
+        self.assertIn("server.2", servers)
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient())
+    def test_srvr(self, client):
+        client = ZooKeeperClient(host="", client_port="")
+        result = client.srvr
+
+        self.assertEqual(set(result.keys()), set(["Zookeeper version", "Outstanding", "Mode"]))
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient())
+    def test_mntr(self, client):
+        client = ZooKeeperClient(host="", client_port="")
+        result = client.mntr
+
+        self.assertEqual(
+            set(result.keys()),
+            set(["zk_pending_syncs", "zk_peer_state"]),
+        )
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient(ready=False))
+    def test_is_ready(self, client):
+        client = ZooKeeperClient(host="", client_port="")
+        self.assertFalse(client.is_ready)
+
+
+class TestManager(unittest.TestCase):
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient(follower=True))
+    def test_init_raises_if_leader_not_found(self, _):
+        with self.assertRaises(QuorumLeaderNotFoundError):
+            ZooKeeperManager(hosts=["host"])
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient())
+    def test_init_finds_leader(self, _):
+        zk = ZooKeeperManager(hosts=["host"])
+        self.assertEqual(zk.leader, "host")
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient(syncing=True))
+    def test_members_syncing(self, _):
+        zk = ZooKeeperManager(hosts=["host"])
+        self.assertTrue(zk.members_syncing)
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient(syncing=True))
+    def test_add_members_raises(self, _):
+        zk = ZooKeeperManager(hosts=["host"])
+        with self.assertRaises(MembersSyncingError):
+            zk.add_members(["server.1=bilbo.baggins"])
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient())
+    @patch.object(DummyClient, "reconfig")
+    def test_add_members_correct_args(self, reconfig, _):
+        zk = ZooKeeperManager(hosts=["server.1=bilbo.baggins"])
+        zk.add_members(["server.2=sam.gamgee"])
+
+        reconfig.assert_called_with(
+            joining="server.2=sam.gamgee", leaving=None, new_members=None, from_config=4294967296
+        )
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient())
+    @patch.object(DummyClient, "reconfig")
+    def test_add_members_runs_on_leader(self, _, client):
+        zk = ZooKeeperManager(hosts=["server.1=bilbo.baggins"])
+        zk.leader = "leader"
+        zk.add_members(["server.2=sam.gamgee"])
+
+        client.assert_called_with(hosts="leader:2181", timeout=5.0)
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient(syncing=True))
+    def test_remove_members_raises(self, _):
+        zk = ZooKeeperManager(hosts=["host"])
+        with self.assertRaises(MembersSyncingError):
+            zk.remove_members(["server.1=bilbo.baggins"])
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient())
+    @patch.object(DummyClient, "reconfig")
+    def test_remove_members_correct_args(self, reconfig, _):
+        zk = ZooKeeperManager(hosts=["server.1=bilbo.baggins"])
+        zk.remove_members(["server.2=sam.gamgee"])
+
+        reconfig.assert_called_with(
+            joining=None, leaving="2", new_members=None, from_config=4294967296
+        )
+
+    @patch("charms.zookeeper.v0.client.KazooClient", return_value=DummyClient())
+    @patch.object(DummyClient, "reconfig")
+    def test_remove_members_runs_on_leader(self, _, client):
+        zk = ZooKeeperManager(hosts=["server.1=bilbo.baggins"])
+        zk.leader = "leader"
+        zk.remove_members(["server.2=sam.gamgee"])
+
+        client.assert_called_with(hosts="leader:2181", timeout=5.0)
---
+++ tests/unit/test_cluster.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import re
+import unittest
+
+import ops.testing
+from charms.zookeeper.v0.cluster import (
+    NotUnitTurnError,
+    UnitNotFoundError,
+    ZooKeeperCluster,
+)
+from ops.charm import CharmBase
+from ops.model import Unit
+from ops.testing import Harness
+
+ops.testing.SIMULATE_CAN_CONNECT = True
+
+
+METADATA = """
+    name: zookeeper
+    peers:
+        cluster:
+            interface: cluster
+"""
+
+
+class DummyZooKeeperCharm(CharmBase):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.cluster = ZooKeeperCluster(self)
+
+
+class TestCluster(unittest.TestCase):
+    def setUp(self):
+        self.harness = Harness(DummyZooKeeperCharm, meta=METADATA)
+        self.addCleanup(self.harness.cleanup)
+        self.relation_id = self.harness.add_relation("cluster", "cluster")
+        self.harness.begin_with_initial_hooks()
+
+    @property
+    def cluster(self):
+        return self.harness.charm.cluster
+
+    def test_peer_units_contains_unit(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+
+        self.assertEqual(len(self.cluster.peer_units), 2)
+
+    def test_started_units_ignores_ready_units(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+        self.harness.update_relation_data(self.relation_id, "zookeeper/1", {"state": "ready"})
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/2")
+        self.harness.update_relation_data(self.relation_id, "zookeeper/2", {"state": "started"})
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/3")
+        self.harness.update_relation_data(self.relation_id, "zookeeper/3", {"state": "started"})
+
+        self.assertEqual(len(self.cluster.started_units), 2)
+
+    def test_get_unit_id(self):
+        self.assertEqual(self.harness.charm.cluster.get_unit_id(self.harness.charm.unit), 0)
+
+    def test_get_unit_from_id_succeeds(self):
+        unit = self.cluster.get_unit_from_id(0)
+
+        self.assertTrue(isinstance(unit, Unit))
+
+    def test_get_unit_from_id_raises(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+
+        with self.assertRaises(UnitNotFoundError):
+            self.cluster.get_unit_from_id(100)
+
+    def test_unit_config_raises_for_missing_unit(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+
+        with self.assertRaises(UnitNotFoundError):
+            self.cluster.get_unit_from_id(100)
+
+    def test_unit_config_succeeds_for_id(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/1", {"private-address": "treebeard"}
+        )
+
+        self.cluster.unit_config(unit=1)
+
+    def test_unit_config_succeeds_for_unit(self):
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+
+        self.cluster.unit_config(self.harness.charm.unit)
+
+    def test_unit_config_has_all_keys(self):
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        config = self.cluster.unit_config(0)
+
+        self.assertEqual(
+            set(config.keys()),
+            set(["host", "server_string", "server_id", "unit_id", "unit_name", "state"]),
+        )
+
+    def test_unit_config_server_string_format(self):
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        server_string = self.cluster.unit_config(0)["server_string"]
+        split_string = re.split("=|:|;", server_string)
+
+        self.assertEqual(len(split_string), 7)
+        self.assertIn("treebeard", split_string)
+
+    def test_get_updated_servers(self):
+        server_strings = [
+            "server.2=gandalf.the.grey",
+            "server.3=in.a.hole.in.the.ground.there.lived.a:hobbit",
+        ]
+        updated_servers = self.cluster._get_updated_servers(
+            server_strings=server_strings, updated_state="ready"
+        )
+
+        self.assertDictEqual(updated_servers, {"2": "ready", "1": "ready"})
+
+    def test_is_unit_turn_succeeds_scaleup(self):
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "added", "1": "added", "2": "added"}
+        )
+
+        self.assertTrue(self.cluster._is_unit_turn(3))
+
+    def test_is_unit_turn_fails_scaleup(self):
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "added", "1": "added"}
+        )
+
+        self.assertFalse(self.cluster._is_unit_turn(3))
+
+    def test_is_unit_turn_succeeds_failover(self):
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "removed", "1": "added"}
+        )
+
+        self.assertTrue(self.cluster._is_unit_turn(0))
+        self.assertTrue(self.cluster._is_unit_turn(2))
+
+    def test_is_unit_turn_fails_failover(self):
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "removed", "1": "added"}
+        )
+
+        self.assertFalse(self.cluster._is_unit_turn(3))
+
+    def test_generate_units_scaleup_adds_all_servers(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/2")
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/1", {"private-address": "gandalf"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/2", {"private-address": "gimli"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "added", "1": "added"}
+        )
+
+        new_unit_string = self.cluster.unit_config(2, state="ready", role="observer")[
+            "server_string"
+        ]
+        generated_servers = self.cluster._generate_units(unit_string=new_unit_string)
+
+        self.assertIn("server.3=gimli", generated_servers)
+        self.assertEqual(len(generated_servers.splitlines()), 4)
+
+    def test_generate_units_scaleup_adds_correct_roles_for_added_units(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/2")
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/1", {"private-address": "gandalf"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/2", {"private-address": "gimli"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "added", "1": "removed"}
+        )
+
+        new_unit_string = self.cluster.unit_config(2, state="ready", role="observer")[
+            "server_string"
+        ]
+        generated_servers = self.cluster._generate_units(unit_string=new_unit_string)
+
+        self.assertEqual(len(re.findall("participant", generated_servers)), 1)
+        self.assertEqual(len(re.findall("observer", generated_servers)), 1)
+
+    def test_generate_units_failover(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/2")
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/1", {"private-address": "gandalf"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "removed", "1": "added", "2": "removed"}
+        )
+
+        new_unit_string = self.cluster.unit_config(0, state="ready", role="observer")[
+            "server_string"
+        ]
+        generated_servers = self.cluster._generate_units(unit_string=new_unit_string)
+
+        self.assertEqual(len(generated_servers.splitlines()), 3)
+
+    def test_ready_to_start_raises_for_missing_data(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/2")
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "removed", "password": "Mellon"}
+        )
+
+        with self.assertRaises(UnitNotFoundError):
+            self.cluster.ready_to_start(unit=2)
+
+    def test_ready_to_start_raises_for_not_unit_turn(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/2")
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/1", {"private-address": "gandalf"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/2", {"private-address": "gimli"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "removed", "password": "Mellon"}
+        )
+
+        with self.assertRaises(NotUnitTurnError):
+            self.cluster.ready_to_start(unit=2)
+
+    def test_ready_to_start_succeeds_init(self):
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        servers, _ = self.cluster.ready_to_start(unit=0)
+        self.assertNotIn("observer", servers)
+
+    def test_ready_to_start_succeeds_failover_after_init(self):
+        self.harness.add_relation_unit(self.relation_id, "zookeeper/1")
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/0", {"private-address": "treebeard"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper/1", {"private-address": "gandalf"}
+        )
+        self.harness.update_relation_data(
+            self.relation_id, "zookeeper", {"0": "removed", "1": "added"}
+        )
+
+        generated_servers, _ = self.cluster.ready_to_start(unit=0)
+
+        self.assertEqual(len(re.findall("participant", generated_servers)), 1)
+        self.assertEqual(len(re.findall("observer", generated_servers)), 1)
