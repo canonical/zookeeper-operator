e8828aeb22d7929e034e27dd527d2fdccc7177fd
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2022-09-06 14:02:01 +0100

    style: fix linting

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -4,21 +4,13 @@
 
 import asyncio
 import logging
-import time
 from pathlib import Path
 
 import pytest
 import yaml
 from pytest_operator.plugin import OpsTest
 
-from tests.integration.helpers import (
-    check_key,
-    check_properties,
-    get_password,
-    ping_servers,
-    restart_unit,
-    write_key,
-)
+from tests.integration.helpers import check_properties, ping_servers
 
 logger = logging.getLogger(__name__)
 
@@ -30,15 +22,15 @@ APP_NAME = METADATA["name"]
 async def test_deploy_ssl_quorum(ops_test: OpsTest):
     charm = await ops_test.build_charm(".")
     await asyncio.gather(
-        ops_test.model.deploy(charm, application_name=APP_NAME, num_units=3),
+        ops_test.model.deploy(charm, application_name=APP_NAME, num_units=1),
         ops_test.model.deploy(
             "tls-certificates-operator",
             application_name="tls-certificates-operator",
+            channel="edge",
             num_units=1,
             config={"generate-self-signed-certificates": "true", "ca-common-name": "zookeeper"},
         ),
     )
-    await ops_test.model.block_until(lambda: len(ops_test.model.applications[APP_NAME].units) == 3)
     await ops_test.model.wait_for_idle(
         apps=[APP_NAME, "tls-certificates-operator"], status="active", timeout=1000
     )
@@ -77,6 +69,7 @@ async def test_add_tls_provider(ops_test: OpsTest):
         ops_test.model.deploy(
             "tls-certificates-operator",
             application_name="tls-certificates-operator",
+            channel="edge",
             num_units=1,
             config={"generate-self-signed-certificates": "true", "ca-common-name": "zookeeper"},
         ),
