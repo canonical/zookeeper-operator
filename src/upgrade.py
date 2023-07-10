0f171d0061537ea9e27793a51c9dee0a5db6ce7c
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-07-10 18:26:22 +0100

    chore: implement pre_upgrade_check, cache client property

--- src/upgrade.py
+++ src/upgrade.py
@@ -3,8 +3,15 @@
 
 """Manager for handling ZooKeeper in-place upgrades."""
 
-from charms.data_platform_libs.v0.upgrade import DataUpgrade, DependencyModel, UpgradeGrantedEvent
-from charms.zookeeper.v0.client import ZooKeeperManager
+from functools import cached_property
+
+from charms.data_platform_libs.v0.upgrade import (
+    ClusterNotReadyError,
+    DataUpgrade,
+    DependencyModel,
+    UpgradeGrantedEvent,
+)
+from charms.zookeeper.v0.client import QuorumLeaderNotFoundError, ZooKeeperManager
 from pydantic import BaseModel
 from typing_extensions import override
 
@@ -26,24 +33,43 @@ class ZooKeeperUpgrade(DataUpgrade):
         super().__init__(charm, **kwargs)
         self.charm = charm
 
-    @override
-    def pre_upgrade_check(self) -> None:
-        raise NotImplementedError
-
-    @override
-    def build_upgrade_stack(self) -> list[int]:
-        zk = ZooKeeperManager(
+    @cached_property
+    def client(self) -> ZooKeeperManager:
+        """Cached client manager application for performing ZK commands."""
+        return ZooKeeperManager(
             hosts=self.charm.cluster.active_hosts,
             client_port=self.charm.cluster.client_port,
             username="super",
             password=self.charm.cluster.passwords[0],
         )
 
+    @override
+    def pre_upgrade_check(self) -> None:
+        default_message = "Pre-upgrade check failed and cannot safely upgrade"
+        try:
+            if self.client.members_not_broadcasting or not len(self.client.server_members) == len(
+                self.charm.cluster.peer_units
+            ):
+                raise ClusterNotReadyError(
+                    message=default_message,
+                    cause="Not all application units are connected and broadcasting in the quorum",
+                )
+
+            if self.client.members_syncing:
+                raise ClusterNotReadyError(
+                    message=default_message, cause="Some quorum members are syncing data"
+                )
+
+        except QuorumLeaderNotFoundError:
+            raise ClusterNotReadyError(message=default_message, cause="Quorum leader not found")
+
+    @override
+    def build_upgrade_stack(self) -> list[int]:
         upgrade_stack = []
         for unit in self.charm.cluster.peer_units:
             config = self.charm.cluster.unit_config(unit=unit)
 
-            if config["host"] == zk.leader:
+            if config["host"] == self.client.leader:
                 upgrade_stack.insert(0, config["unit_id"])
             else:
                 upgrade_stack.append(config["unit_id"])
