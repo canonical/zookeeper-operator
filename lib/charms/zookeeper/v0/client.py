44ba33645406f3128ff9a7f60672c0d7d7ca67c4
Raul Zamora <raul.zamora@canonical.com>
2023-08-04 15:00:14 +0200

    add version getter

--- lib/charms/zookeeper/v0/client.py
+++ lib/charms/zookeeper/v0/client.py
@@ -74,7 +74,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 6
+LIBPATCH = 7
 
 
 logger = logging.getLogger(__name__)
@@ -424,6 +424,24 @@ class ZooKeeperManager:
             certfile_path=self.certfile_path,
         ) as zk:
             zk.delete_znode(path=path)
+
+    def get_version(self) -> str:
+        """Get ZooKeeper version.
+
+        Version will have a similar format to
+            3.6.4--d65253dcf68e9097c6e95a126463fd5fdeb4521c, built on 12/18/2022 18:10 GMT
+        """
+        with ZooKeeperClient(
+            host=self.leader,
+            client_port=self.client_port,
+            username=self.username,
+            password=self.password,
+            use_ssl=self.use_ssl,
+            keyfile_path=self.keyfile_path,
+            keyfile_password=self.keyfile_password,
+            certfile_path=self.certfile_path,
+        ) as zk:
+            return zk.srvr["Zookeeper version"]
 
 
 class ZooKeeperClient:
