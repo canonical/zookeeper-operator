d075b3b44928e18fd71897b62a624b7f62f9c84f
marcoppenheimer <51744472+marcoppenheimer@users.noreply.github.com>
2023-08-21 16:48:55 +0100

    DPE-2357 - test: add freeze-db ha test (#81)

---
+++ tests/integration/ha/continuous_writes.py
@@ -0,0 +1,103 @@
+import logging
+import subprocess
+import sys
+import time
+
+from kazoo.client import KazooClient
+from kazoo.retry import KazooRetry
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+def continous_writes(parent: str, hosts: str, username: str, password: str):
+    count = 0
+
+    while True:
+        client = KazooClient(
+            hosts=hosts,
+            timeout=10,
+            sasl_options={"mechanism": "DIGEST-MD5", "username": username, "password": password},
+            connection_retry=KazooRetry(max_tries=10, delay=1),
+            command_retry=KazooRetry(max_tries=10, delay=1),
+        )
+        client.start()
+
+        client.create(f"{parent}/{count}", acl=[], makepath=True)
+
+        client.stop()
+
+        time.sleep(1)
+        count += 1
+
+
+def start_continuous_writes(parent: str, hosts: str, username: str, password: str):
+    subprocess.Popen(
+        [
+            "python3",
+            "tests/integration/ha/continuous_writes.py",
+            parent,
+            hosts,
+            username,
+            password,
+        ]
+    )
+
+
+def stop_continuous_writes():
+    proc = subprocess.Popen(["pkill", "-9", "-f", "continous_writes.py"])
+    proc.communicate()
+
+
+def get_last_znode(parent: str, hosts: str, username: str, password: str) -> int:
+    client = KazooClient(
+        hosts=hosts,
+        timeout=5,
+        sasl_options={"mechanism": "DIGEST-MD5", "username": username, "password": password},
+        connection_retry=KazooRetry(max_tries=10, delay=1),
+        command_retry=KazooRetry(max_tries=10, delay=1),
+        read_only=True,
+    )
+    client.start()
+
+    znodes = client.get_children(parent)
+    assert znodes
+
+    last_znode = sorted((int(x) for x in znodes))[-1]
+
+    client.stop()
+
+    return last_znode
+
+
+def count_znodes(parent: str, hosts: str, username: str, password: str) -> int:
+    client = KazooClient(
+        hosts=hosts,
+        timeout=5,
+        sasl_options={"mechanism": "DIGEST-MD5", "username": username, "password": password},
+        connection_retry=KazooRetry(max_tries=10, delay=1),
+        command_retry=KazooRetry(max_tries=10, delay=1),
+        read_only=True,
+    )
+    client.start()
+
+    znodes = client.get_children(parent)
+
+    assert znodes
+
+    client.stop()
+
+    return len(znodes) - 1  # to account for the parent node
+
+
+def main():
+    parent = sys.argv[1]
+    hosts = sys.argv[2]
+    username = sys.argv[3]
+    password = sys.argv[4]
+
+    continous_writes(parent, hosts, username, password)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/integration/ha/helpers.py
@@ -0,0 +1,108 @@
+import logging
+import re
+import subprocess
+from pathlib import Path
+
+import yaml
+from pytest_operator.plugin import OpsTest
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+logger = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+APP_NAME = METADATA["name"]
+
+PROCESS = "org.apache.zookeeper.server.quorum.QuorumPeerMain"
+
+
+@retry(
+    wait=wait_fixed(5),
+    stop=stop_after_attempt(3),
+    reraise=True,
+)
+def srvr(host: str) -> dict:
+    response = subprocess.check_output(
+        f"echo srvr | nc {host} 2181", stderr=subprocess.PIPE, shell=True, universal_newlines=True
+    )
+
+    assert response, "ZooKeeper not running"
+
+    result = {}
+    for item in response.splitlines():
+        k = re.split(": ", item)[0]
+        v = re.split(": ", item)[1]
+        result[k] = v
+
+    return result
+
+
+async def get_hosts(ops_test: OpsTest, app_name: str = APP_NAME, port: int = 2181) -> str:
+    return ",".join(
+        [
+            f"{unit.public_address}:{str(port)}"
+            for unit in ops_test.model.applications[app_name].units
+        ]
+    )
+
+
+async def get_unit_host(
+    ops_test: OpsTest, unit_name: str, app_name: str = APP_NAME, port: int = 2181
+) -> str:
+    return [
+        f"{unit.public_address}:{str(port)}"
+        for unit in ops_test.model.applications[app_name].units
+        if unit.name == unit_name
+    ][0]
+
+
+async def get_leader_name(ops_test: OpsTest, app_name: str = APP_NAME) -> str:
+    logger.info("Getting leader name...")
+    for unit in ops_test.model.applications[app_name].units:
+        host = unit.public_address
+        logger.info(f"{unit.name=} + {host=}")
+        try:
+            mode = srvr(host)["Mode"]
+            logger.info(f"{mode=}")
+        except subprocess.CalledProcessError:  # unit is down
+            continue
+        if mode == "leader":
+            return unit.name
+
+    raise Exception("Leader not found")
+
+
+def get_super_password(ops_test: OpsTest, app_name: str = APP_NAME) -> str:
+    for unit in ops_test.model.applications[app_name].units:
+        show_unit = subprocess.check_output(
+            f"JUJU_MODEL={ops_test.model_full_name} juju show-unit {unit.name}",
+            stderr=subprocess.PIPE,
+            shell=True,
+            universal_newlines=True,
+        )
+        response = yaml.safe_load(show_unit)
+        relations_info = response[f"{unit.name}"]["relation-info"]
+
+        password = None
+        for info in relations_info:
+            if info["endpoint"] == "cluster":
+                password = info["application-data"]["super-password"]
+                return password
+
+        if not password:
+            raise Exception("no relations found")
+
+
+async def kill_unit_process(
+    ops_test: OpsTest, unit_name: str, kill_code: str, app_name: str = APP_NAME
+) -> None:
+    if len(ops_test.model.applications[app_name].units) < 3:
+        await ops_test.model.applications[app_name].add_unit(count=1)
+        await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
+
+    kill_cmd = f"run --unit {unit_name} -- pkill --signal {kill_code} -f {PROCESS}"
+    return_code, _, _ = await ops_test.juju(*kill_cmd.split())
+
+    if return_code != 0:
+        raise Exception(
+            f"Expected kill command {kill_cmd} to succeed instead it failed: {return_code}"
+        )
