95fbb16deae4179df3436a606b1b52ae2a1c47d6
Raul Zamora <raul.zamora@canonical.com>
2023-09-20 13:33:44 +0200

    add leader restart test

--- tests/integration/ha/test_ha.py
+++ tests/integration/ha/test_ha.py
@@ -95,6 +95,68 @@ async def test_kill_db_process(ops_test: OpsTest, request):
     logger.info("Killing leader process...")
     await helpers.send_control_signal(ops_test=ops_test, unit_name=leader_name, signal="SIGKILL")
 
+    # Check that process is down
+    assert await helpers.is_down(ops_test=ops_test, unit=leader_name)
+
+    await asyncio.sleep(RESTART_DELAY * 2)
+
+    logger.info("Checking writes are increasing...")
+    writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=USERNAME, password=password
+    )
+    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # increasing writes
+    new_writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=USERNAME, password=password
+    )
+    assert new_writes > writes, "writes not continuing to ZK"
+
+    logger.info("Checking leader re-election...")
+    new_leader_name = helpers.get_leader_name(ops_test, non_leader_hosts)
+    assert new_leader_name != leader_name
+
+    logger.info("Stopping continuous_writes...")
+    cw.stop_continuous_writes()
+
+    logger.info("Counting writes on surviving units...")
+    last_write = cw.get_last_znode(
+        parent=parent, hosts=non_leader_hosts, username=USERNAME, password=password
+    )
+    total_writes = cw.count_znodes(
+        parent=parent, hosts=non_leader_hosts, username=USERNAME, password=password
+    )
+    assert last_write == total_writes
+
+    logger.info("Checking old leader caught up...")
+    last_write_leader = cw.get_last_znode(
+        parent=parent, hosts=leader_host, username=USERNAME, password=password
+    )
+    total_writes_leader = cw.count_znodes(
+        parent=parent, hosts=leader_host, username=USERNAME, password=password
+    )
+    assert last_write == last_write_leader
+    assert total_writes == total_writes_leader
+
+
+@pytest.mark.abort_on_fail
+async def test_restart_db_process(ops_test: OpsTest, request):
+    """SIGTERMSs leader process and checks recovery + re-election."""
+    hosts = helpers.get_hosts(ops_test)
+    leader_name = helpers.get_leader_name(ops_test, hosts)
+    leader_host = helpers.get_unit_host(ops_test, leader_name)
+    password = helpers.get_super_password(ops_test)
+    parent = request.node.name
+    non_leader_hosts = ",".join([host for host in hosts.split(",") if host != leader_host])
+
+    logger.info("Starting continuous_writes...")
+    cw.start_continuous_writes(parent=parent, hosts=hosts, username=USERNAME, password=password)
+    await asyncio.sleep(CLIENT_TIMEOUT * 3)  # letting client set up and start writing
+
+    logger.info("Checking writes are running at all...")
+    assert cw.count_znodes(parent=parent, hosts=hosts, username=USERNAME, password=password)
+
+    logger.info("Killing leader process...")
+    await helpers.send_control_signal(ops_test=ops_test, unit_name=leader_name, signal="SIGTERM")
+
     logger.info("Checking writes are increasing...")
     writes = cw.count_znodes(
         parent=parent, hosts=non_leader_hosts, username=USERNAME, password=password
