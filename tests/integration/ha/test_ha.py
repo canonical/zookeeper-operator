a9abee9b175a90ce8debabdf6ade0f54722780f1
Raul Zamora <raul.zamora@canonical.com>
2023-09-25 17:37:11 +0200

    fix get password

--- tests/integration/ha/test_ha.py
+++ tests/integration/ha/test_ha.py
@@ -49,8 +49,7 @@ async def test_replication(ops_test: OpsTest):
     host_0 = ops_test.model.applications[APP_NAME].units[0].public_address
     host_1 = ops_test.model.applications[APP_NAME].units[1].public_address
     host_2 = ops_test.model.applications[APP_NAME].units[2].public_address
-    model_full_name = ops_test.model_full_name
-    password = helpers.get_password(model_full_name or "")
+    password = helpers.get_password(ops_test=ops_test)
 
     helpers.write_key(host=host_0, password=password)
     await asyncio.sleep(1)
@@ -61,7 +60,7 @@ async def test_replication(ops_test: OpsTest):
 
 
 @pytest.mark.abort_on_fail
-async def test_kill_db_process(ops_test: OpsTest, request):
+async def test_kill_db_process(ops_test: OpsTest, request, restart_delay):
     """SIGKILLs leader process and checks recovery + re-election."""
     hosts = helpers.get_hosts(ops_test)
     leader_name = helpers.get_leader_name(ops_test, hosts)
