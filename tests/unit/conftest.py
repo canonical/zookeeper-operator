8ba54546942d3bd601784d3a2c1c8953b08fcb72
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-10-17 00:16:24 +0100

    chore: patch out set_etc_hosts in unit tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,9 +1,17 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+from unittest.mock import patch
+
 import pytest
 
 
 @pytest.fixture(autouse=True)
 def patched_wait(mocker):
     mocker.patch("tenacity.nap.time")
+
+
+@pytest.fixture(autouse=True)
+def patched_etc_hosts():
+    with patch("config.ZooKeeperConfig.set_etc_hosts"):
+        yield
