7e47a0bcac3a2e8d99737f1d2c13467f3d246fe6
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-01-25 14:42:39 +0000

    test: add unit-tests for chroot fixes

--- tests/unit/test_quorum.py
+++ tests/unit/test_quorum.py
@@ -4,13 +4,14 @@
 
 import logging
 from pathlib import Path
+from unittest.mock import DEFAULT, MagicMock, patch
 
 import pytest
 import yaml
 from ops.testing import Harness
 
 from charm import ZooKeeperCharm
-from literals import CHARM_KEY
+from literals import CHARM_KEY, PEER, REL_NAME
 
 logger = logging.getLogger(__name__)
 
@@ -22,9 +23,10 @@ METADATA = str(yaml.safe_load(Path("./metadata.yaml").read_text()))
 @pytest.fixture
 def harness():
     harness = Harness(ZooKeeperCharm, meta=METADATA, config=CONFIG, actions=ACTIONS)
-    harness.add_relation("restart", CHARM_KEY)
     upgrade_rel_id = harness.add_relation("upgrade", CHARM_KEY)
+    harness.add_relation("restart", CHARM_KEY)
     harness.update_relation_data(upgrade_rel_id, f"{CHARM_KEY}/0", {"state": "idle"})
+    harness.add_relation(PEER, CHARM_KEY)
     harness._update_config({"init-limit": 5, "sync-limit": 2, "tick-time": 2000})
     harness.begin()
     return harness
@@ -57,3 +59,52 @@ def test_is_child_of_not(harness):
     chroots = {"/gandalf", "/saruman"}
 
     assert not harness.charm.quorum_manager._is_child_of(path=chroot, chroots=chroots)
+
+
+def test_update_acls_correctly_handles_relation_chroots(harness):
+    dummy_leader_znodes = {
+        "/fellowship",
+        "/fellowship/men",
+        "/fellowship/dwarves",
+        "/fellowship/men/aragorn",
+        "/fellowship/men/boromir",
+        "/fellowship/elves/legolas",
+        "/fellowship/dwarves/gimli",
+        "/fellowship/men/aragorn/anduril",
+    }
+
+    with harness.hooks_disabled():
+        app_id = harness.add_relation(REL_NAME, "application")
+        harness.update_relation_data(app_id, "application", {"chroot": "/rohan"})
+        harness.set_leader(True)
+
+    with patch.multiple(
+        "charms.zookeeper.v0.client.ZooKeeperManager",
+        get_leader=DEFAULT,
+        leader_znodes=MagicMock(return_value=dummy_leader_znodes),
+        create_znode_leader=DEFAULT,
+        set_acls_znode_leader=DEFAULT,
+        delete_znode_leader=DEFAULT,
+    ) as patched_manager:
+        harness.charm.quorum_manager.update_acls()
+
+        for _, kwargs in patched_manager["create_znode_leader"].call_args_list:
+            assert "/rohan" in kwargs["path"]
+
+        for _, kwargs in patched_manager["set_acls_znode_leader"].call_args_list:
+            assert "/rohan" in kwargs["path"]
+
+        removed_men = False
+        for counter, call in enumerate(patched_manager["delete_znode_leader"].call_args_list):
+            _, kwargs = call
+
+            if "/fellowship/men" in kwargs["path"]:
+                assert not removed_men, "Parent zNode removed before all it's children"
+
+            if kwargs["path"] == "/fellowship/men":
+                removed_men = True
+
+        # ensure last node to go is the parent
+        assert (
+            patched_manager["delete_znode_leader"].call_args_list[-1][1]["path"] == "/fellowship"
+        )
