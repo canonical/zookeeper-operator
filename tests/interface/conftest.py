0c2ba23a56b86d16cd9ad02973c30a0cd1751634
Alex Batisse <alex.batisse@canonical.com>
2024-04-24 12:02:08 +0200

    fix (tests): Typo

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -1,9 +1,10 @@
 import pytest
-from charm import ZookeeperCharm
 from interface_tester.plugin import InterfaceTester
+
+from charm import ZooKeeperCharm
 
 
 @pytest.fixture
 def interface_tester(interface_tester: InterfaceTester):
-    interface_tester.configure(charm_type=ZookeeperCharm)
+    interface_tester.configure(charm_type=ZooKeeperCharm)
     yield interface_tester
