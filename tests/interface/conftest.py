e0401d36ea02c2b1b96ca628413fda44d3d9805e
Alex Batisse <alex.batisse@canonical.com>
2024-04-25 15:31:34 +0200

    WIP: Use repo with ZK interface

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -5,5 +5,9 @@ from interface_tester.plugin import InterfaceTester
 
 @pytest.fixture
 def interface_tester(interface_tester: InterfaceTester):
-    interface_tester.configure(charm_type=ZooKeeperCharm)
+    interface_tester.configure(
+        charm_type=ZooKeeperCharm,
+        repo="https://github.com/Batalex/charm-relation-interfaces",
+        branch="feat/DPE-3737",
+    )
     yield interface_tester
