016fda9722a36a661bbbc639841a6a0a2c7a62cf
Alex Batisse <alex.batisse@canonical.com>
2024-04-26 16:01:30 +0200

    tests: Add interface test

--- tests/interface/test_inferface.py
+++ tests/interface/test_inferface.py
@@ -2,4 +2,9 @@ from interface_tester.plugin import InterfaceTester
 
 
 def test_interface(interface_tester: InterfaceTester):
-    pass
+    interface_tester.configure(
+        interface_name="zookeeper_client",
+        interface_version=0
+    )
+    interface_tester.run()
+
