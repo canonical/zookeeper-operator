9d3f68d63761c09743bc1315d2141df89debbb04
Alex Batisse <alex.batisse@canonical.com>
2024-04-29 16:25:01 +0200

    fix: Remove leftover 'zookeeper-client'

--- tests/interface/test_inferface.py
+++ tests/interface/test_inferface.py
@@ -2,5 +2,5 @@ from interface_tester.plugin import InterfaceTester
 
 
 def test_interface(interface_tester: InterfaceTester):
-    interface_tester.configure(interface_name="zookeeper_client", interface_version=0)
+    interface_tester.configure(interface_name="zookeeper", interface_version=0)
     interface_tester.run()
