315c351ab1cfc7ca9bf6efb021b23f306f932615
Alex Batisse <alex.batisse@canonical.com>
2024-08-27 18:45:08 +0200

    feat: Create bucket or use existing upon s3 credential change

--- src/events/backup.py
+++ src/events/backup.py
@@ -3,6 +3,7 @@
 # See LICENSE file for licensing details.
 
 """Event handlers for creating and restoring backups."""
+import json
 import logging
 from typing import TYPE_CHECKING
 
@@ -13,7 +14,8 @@ from charms.data_platform_libs.v0.s3 import (
 )
 from ops.framework import Object
 
-from literals import S3_REL_NAME
+from literals import S3_BACKUPS_PATH, S3_REL_NAME, Status
+from managers.backup import BackupManager
 
 if TYPE_CHECKING:
     from charm import ZooKeeperCharm
@@ -27,33 +29,68 @@ class BackupEvents(Object):
     def __init__(self, charm):
         super().__init__(charm, "backup")
         self.charm: "ZooKeeperCharm" = charm
-        self.s3_client = S3Requirer(self.charm, S3_REL_NAME)
+        self.s3_requirer = S3Requirer(self.charm, S3_REL_NAME)
+        self.backup_manager = BackupManager()
 
         self.framework.observe(
-            self.s3_client.on.credentials_changed, self._on_s3_credentials_changed
+            self.s3_requirer.on.credentials_changed, self._on_s3_credentials_changed
         )
-        self.framework.observe(self.s3_client.on.credentials_gone, self._on_s3_credentials_gone)
+        self.framework.observe(self.s3_requirer.on.credentials_gone, self._on_s3_credentials_gone)
 
         self.framework.observe(self.charm.on.create_backup_action, self._on_create_backup_action)
         self.framework.observe(self.charm.on.list_backups_action, self._on_list_backups_action)
         self.framework.observe(self.charm.on.restore_action, self._on_restore_action)
 
     def _on_s3_credentials_changed(self, event: CredentialsChangedEvent):
-        # TODO:
-        # - store credentials
-        # - create bucket if it does not exist
-
         if not self.charm.unit.is_leader():
             return
 
-        _credentials = self.s3_client.get_s3_connection_info()
+        if not self.charm.state.peer_relation:
+            self.charm._set_status(Status.NO_PEER_RELATION)
+            event.defer()
+            return
+
+        s3_parameters = self.s3_requirer.get_s3_connection_info()
+        required_parameters = [
+            "bucket",
+            "access-key",
+            "secret-key",
+        ]
+        missing_required_parameters = [
+            param for param in required_parameters if param not in s3_parameters
+        ]
+        if missing_required_parameters:
+            logger.warning(
+                f"Missing required S3 parameters in relation with S3 integrator: {missing_required_parameters}"
+            )
+            self.charm._set_status(Status.SERVICE_NOT_INSTALLED)
+            return
+
+        s3_parameters = self.s3_requirer.get_s3_connection_info()
+
+        # Add some sensible defaults (as expected by the code) for missing optional parameters
+        s3_parameters.setdefault("endpoint", "https://s3.amazonaws.com")
+        s3_parameters.setdefault("region", "")
+        s3_parameters.setdefault("path", S3_BACKUPS_PATH)
+        s3_parameters.setdefault("s3-uri-style", "host")
+        s3_parameters.setdefault("delete-older-than-days", "9999999")
+
+        # Strip whitespaces from all parameters.
+        for key, value in s3_parameters.items():
+            if isinstance(value, str):
+                s3_parameters[key] = value.strip()
+
+        if not self.backup_manager.create_bucket(s3_parameters):
+            self.charm._set_status(Status.BUCKET_NOT_CREATED)
+            return
+
+        self.charm.state.cluster.update({"s3-credentials": json.dumps(s3_parameters)})
 
     def _on_s3_credentials_gone(self, event: CredentialsGoneEvent):
-        # TODO: remove credentials from peer data
         if not self.charm.unit.is_leader():
             return
 
-        pass
+        self.charm.state.cluster.update({"s3-credentials": ""})
 
     def _on_create_backup_action(self, _):
         # TODO
---
+++ src/managers/backup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Helpers for managing backups."""
+
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+
+class BackupManager:
+    """Manager for all things backup-related."""
+
+    def __init__(self) -> None:
+        pass
+
+    def create_bucket(self, s3_parameters) -> bool:
+        """Create bucket if it does not exists."""
+        session = boto3.Session(
+            aws_access_key_id=s3_parameters["access-key"],
+            aws_secret_access_key=s3_parameters["secret-key"],
+            region_name=s3_parameters["region"] if s3_parameters["region"] else None,
+        )
+
+        s3 = session.resource("s3")
+        bucket_name = s3_parameters["bucket"]
+        bucket_exists = True
+
+        bucket = s3.Bucket(bucket_name)  # pyright: ignore [reportAttributeAccessIssue]
+
+        try:
+            bucket.meta.client.head_bucket(Bucket=bucket_name)
+        except ClientError as ex:
+            if "(403)" in ex.args[0]:
+                logger.error("Wrong credentials or access to bucket is forbidden")
+                return False
+            elif "(404)" in ex.args[0]:
+                bucket_exists = False
+        else:
+            logger.info(f"Using existing bucket {bucket_name}")
+
+        if not bucket_exists:
+            bucket.create()
+            bucket.wait_until_exists()
+            logger.info(f"Created bucket {bucket_name}")
+
+        return True
