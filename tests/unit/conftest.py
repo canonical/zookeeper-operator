b6fc4308a91354b5b2ac32ba26094a25e7525ae3
Alex Batisse <alex.batisse@canonical.com>
2025-01-06 11:49:47 +0100

    chore: Update workflows to 24.0.0

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-from unittest.mock import Mock, patch, PropertyMock
+from unittest.mock import Mock, PropertyMock, patch
 
 import pytest
 from ops import JujuVersion
