c8111a34f801dd1b502f666f0bcdd420bca90809
Vladimir <48120135+izmalk@users.noreply.github.com>
2025-04-01 09:23:09 +0100

    Enable page edit and sequential nav features

--- docs/conf.py
+++ docs/conf.py
@@ -138,13 +138,11 @@ html_context = {
     'repo_default_branch': 'main',
     # Docs location in the repo; used in links for viewing the source files
     #
-
-
     # TODO: To customise the directory, uncomment and update as needed.
     "repo_folder": "/docs/",
     # TODO: To enable or disable the Previous / Next buttons at the bottom of pages
     # Valid options: none, prev, next, both
-    # "sequential_nav": "both",
+    "sequential_nav": "both",
     # TODO: To enable listing contributors on individual pages, set to True
     "display_contributors": False,
 
@@ -153,6 +151,17 @@ html_context = {
 
     # Required for the Edit the page button
     "top_of_page_buttons": ["edit"],
+}
+
+# TODO: To enable the edit button on pages, uncomment and change the link to a
+# public repository on GitHub or Launchpad. Any of the following link domains
+# are accepted:
+# - https://github.com/example-org/example"
+# - https://launchpad.net/example
+# - https://git.launchpad.net/example
+
+html_theme_options = {
+'source_edit_link': 'https://github.com/canonical/zookeeper-operator',
 }
 
 # Project slug; see https://meta.discourse.org/t/what-is-category-slug/87897
